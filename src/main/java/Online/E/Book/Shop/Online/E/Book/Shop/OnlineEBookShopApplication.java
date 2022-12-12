5f0f022240077ee45dea635abe9b9884d3f32bb8
Sirish <Sirish.mysis77@gmail.com>
2022-12-12 05:51:22 +0000

    adding locig and test

---
+++ src/main/java/Online/E/Book/Shop/Online/E/Book/Shop/OnlineEBookShopApplication.java
@@ -0,0 +1,13 @@
+package Online.E.Book.Shop.Online.E.Book.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEBookShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEBookShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Online/E/Book/Shop/Online/E/Book/Shop/OnlineEBookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Online.E.Book.Shop.Online.E.Book.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEBookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
